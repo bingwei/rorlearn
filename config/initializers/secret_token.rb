3faf9ce0a358bdc72019892654e1a9a74078f6a8
Bing Wei <biwei@paypal.com>
2013-09-10 11:29:06 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Techlearn::Application.config.secret_key_base = 'e2001a3d9778ec47938fec98504d03eabce96719ebb41d85bba9537c89b7eeed95c5ae7c42b96bb7c7c1dbec1a2456d4afbd4fac2175e0c4c192ef002cb8a605'
