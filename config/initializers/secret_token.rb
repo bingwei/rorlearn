6f000e21c9f370df02b29dcbce79bf7f904a9e21
bing.wei <bing.wei@symbio.com>
2013-09-14 20:26:10 +0800

    init rspec

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Heroku::Application.config.secret_key_base = '67cabe3e5cb26d0ac7713c44a273783ea413149d9c5c18c2653def737fbcd0d013fef286aafd8f70925f019ba692e43f91d04ee304490c22b36eceebde2fe20e'
+# '67cabe3e5cb26d0ac7713c44a273783ea413149d9c5c18c2653def737fbcd0d013fef286aafd8f70925f019ba692e43f91d04ee304490c22b36eceebde2fe20e'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    File.read(token_file).chomp
+  else
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Heroku::Application.config.secret_key_base = secure_token 
