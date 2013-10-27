0c73d80c58cd95be07ad4dd1c71f1c48e16329b1
bing.wei <bing.wei@symbio.com>
2013-10-27 11:25:31 +0800

    finish practice of user modeling chapter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
   has_many :microposts
   
-  before_save {self.email = email.downcase}
+  # before_save {self.email = email.downcase}
+  before_save {email.downcase!}
   validates(:name, presence: true, length: {maximum: 50})
   
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\Z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+(\.[a-z])*\.[a-z]+\Z/i
   validates(:email, presence: true, format:{with: VALID_EMAIL_REGEX}, 
               uniqueness: {case_sensitive: false})
   
