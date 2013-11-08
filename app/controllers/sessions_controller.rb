78159bb488e88d50b53cfafd2739382f0718737f
bing.wei <bing.wei@symbio.com>
2013-11-08 08:54:38 +0800

    not finished

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_back_to user
     else
       flash.now[:error] = 'Invalid email/password conbination' #Not quite right
       render 'new'
